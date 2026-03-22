68bad34edc93b59e92b890d40aad17ac1bcff4cd
Youichi <46785068+youichiuehara14@users.noreply.github.com>
2026-03-22 10:57:24 +0800

    feat: add filter method reviewer

---
+++ javascript/filter/script.js
@@ -0,0 +1,28 @@
+/* 
+The Intended Design: Return a new array with only items that pass a condition (true/false).
+When to Use It (The Green Light):
+1. Searching (e.g., active users)
+2. Removing unwanted data (e.g., deleted items)
+Common Misuse (The Red Light):
+Forgetting to return a boolean → results become empty or wrong
+Using it when you only need one item → should use find()
+Learning Lab Example:
+*/
+
+const employees = [
+  { name: 'Karina', role: 'dev' },
+  { name: 'Felix', role: 'marketing' },
+  { name: 'BangChan', role: 'dev' },
+  { name: 'Winter', role: 'project manager' },
+  { name: 'Giselle', role: 'dev' },
+];
+
+// WRONG: missing return
+// const wrong = users.filter((user) => {
+//   user.active;
+// });
+// console.log(wrong); // []
+
+// RIGHT:
+const devEmployees = employees.filter((dev) => dev.role === 'dev');
+console.log(devEmployees);
