7093a802df672f3566cf91f122c44defcb169176
Youichi <46785068+youichiuehara14@users.noreply.github.com>
2026-03-24 11:13:55 +0800

    feat: add split method reviewer

---
+++ javascript/split/script.js
@@ -0,0 +1,14 @@
+const testWords =
+  "Touch my body tender 'Cause the feeling makes me weak Kicking off the covers I see the ceiling while you're looking down at me How can we go back to being friends When we just shared a bed? How can you look at me and pretend I'm someone you've never met?";
+
+const splitWords = testWords.split(' ');
+
+console.log(splitWords);
+
+/* you'll get the output of: 
+================================
+  ['Touch', 'my', 'body', 'tender', '\'Cause', 'the', 'feeling', 'makes', 'me', 'weak', 'Kicking', 'off', 'the', 'covers', 'I', 'see', 'the',
+  'ceiling', 'while', 'you\'re', 'looking', 'down', 'at', 'me', 'How', 'can', 'we', 'go', 'back', 'to', 'being', 'friends', 'When', 'we', 'just',
+  'shared', 'a', 'bed?', 'How', 'can', 'you', 'look', 'at', 'me', 'and', 'pretend', 'I\'m', 'someone', 'you\'ve', 'never', 'met?']
+================================
+*/
